6221522f35f82e6631144079a151a84e3418d1b4
yuta higa <yh19950420@gmail.com>
2021-11-03 14:48:04 +0900

    新規投稿画面作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,34 @@
 class ArticlesController < ApplicationController
     def index
+      @articles = Article.all
     end
     
     def show
     end
 
     def new 
+        @article = Article.new
     end
 
     def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+          redirect_to articles_path, notice: '投稿しました'
+        else
+          flash.now[:alert] = '投稿に失敗しました'
+          render :new
+        end
+        @total_minutes = current_user.articles.sum(:minutes)
+        @user = User.find(current_user.id)
+        @user.level = @total_minutes / 30
+        @user.update(level: @user.level)
     end
+    
 
     def destroy
     end
 
     def edit 
     end
-    
+
 end
--- db/migrate/20211031100735_create_articles.rb
+++ db/migrate/20211031100735_create_articles.rb
@@ -1,14 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
       t.date :day, null: false
       t.integer :minutes, null: false
       t.text :body, null: false
-      
-      t.references :user, null: false, foreign_key: true
-
       t.timestamps
-
     end
   end
 end
