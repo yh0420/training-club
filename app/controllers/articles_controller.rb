ea9b851cc4c4bc652d084bf1ed24b539566e5771
yuta higa <yh19950420@gmail.com>
2021-11-15 17:49:49 +0900

    create修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
     end
   
     def create
-        @article = Article.create(article_params)
+        @article = Article.create!(article_params)
         if @article.save
           redirect_to article_path(@article)
         else
