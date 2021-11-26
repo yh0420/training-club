090f8ed6694eee65100cb2042b23d952bf890d28
yuta higa <yh19950420@gmail.com>
2021-11-26 17:45:58 +0900

    プロフィール画像を変更できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me, :objective]
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me, :objective, :avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
