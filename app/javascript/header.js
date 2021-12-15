5969fea203d6535c13fc47122c94d6407e19782b
yuta higa <yh19950420@gmail.com>
2021-12-15 22:05:21 +0900

    ヘッダーをレスポンシブ対応した

---
+++ app/javascript/header.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".nav-button").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(".nav-wrap")
+          .addClass("close")
+          .removeClass("open");
+      } else {
+        $(this).addClass("active");
+        $(".nav-wrap")
+          .addClass("open")
+          .removeClass("close");
+      }
+    });
+  });
