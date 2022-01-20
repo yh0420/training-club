65bf50fecdcbd23b2972b8b519045c8b07f5aa00
yuta higa <yh19950420@gmail.com>
2022-01-21 00:20:31 +0900

    unicorn.rbを変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-rails_root = File.expand_path('../', FILE)
+rails_root = File.expand_path('../../', __FILE__)
 
 worker_processes 2
 working_directory rails_root
@@ -8,4 +8,3 @@ pid "#{rails_root}/tmp/unicorn.pid"
 
 stderr_path "#{rails_root}/log/unicorn_error.log"
 stdout_path "#{rails_root}/log/unicorn.log"
-
