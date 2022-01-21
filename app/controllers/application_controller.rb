bc31c85d9d659057867583aafac167e392cd8afc
yuta higa <yh19950420@gmail.com>
2022-01-21 11:47:23 +0900

    application.controller.rbを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me, :objective, :avatar]
+    added_attrs = [:email, :password, :password_confirmation, :remember_me, :objective, :avatar, :name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
