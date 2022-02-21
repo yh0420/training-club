1f25307de480138a1c58b69bbd3e44a6ee314fbe
yuta higa <yh19950420@gmail.com>
2022-02-21 22:50:35 +0900

    scssフォルダを移動

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 require('jquery')
 //require('jquery_ujs')
 require('./header')
+import "packs/application.scss";
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
