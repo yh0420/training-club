960afd357e2b6aa186cbc2d6e48444fef3e0adbf
yuta higa <yh19950420@gmail.com>
2022-03-20 12:26:48 +0900

    likescontrollerを変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -24,15 +24,13 @@ class LikesController < ApplicationController
     end
 
   def destroy
-    current_user.likes.find_by(article_id: params[:article_id]).destroy!
-    #if current_user.likes.find_by(article_id: params[:article_id]).destroy!
-      #respond_to do |format|
-        #format.any
-        #format.html { redirect_to controller: :articles, action: :show, id: params[:article_id] }
-        #format.js
-      #end
-    #end
-    @article = Article.find(params[:article_id])
+    if current_user.likes.find_by(article_id: params[:article_id]).destroy!
+      respond_to do |format|
+        format.any
+        format.html { redirect_to controller: :articles, action: :show, id: params[:article_id] }
+        format.js
+      end
+    end
   end
 
 
