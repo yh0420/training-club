274032c4bce6fe3c1791b594ce71e23a6e39b5a5
yuta higa <yh19950420@gmail.com>
2022-03-20 10:09:13 +0900

    いいね機能をgetに変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     
     user = User.find(1)  #ゲストユーザー（id:28）にログインさせている
-    user.update(email: 'guest@example.com',name: 'ゲストユーザー') do |user|
+    user.update(email: 'guest@example.com', name: 'ゲストユーザー') do |user|
     user.password = SecureRandom.urlsafe_base64
   end
     sign_in user  #ユーザーにログインさせる
