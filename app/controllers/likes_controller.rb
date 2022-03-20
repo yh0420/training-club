25109b768f3544f7b99aebdd3cd6c55a68e357dd
yuta higa <yh19950420@gmail.com>
2022-03-20 10:40:46 +0900

    likescontrollerを変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,20 @@ class LikesController < ApplicationController
 
       def create
         current_user.likes.create!(article_id: params[:article_id])
+        if current_user.likes.create!(article_id: params[:article_id])
+          respond_to do |format|
+            #format.html { redirect_to controller: :accounts, action: :show, id: @user.id }
+            format.js
+          end
+        end
         @article = Article.find(params[:article_id])
+        #respond_to do |format|
+         # unless following.save
+          #  flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+          #end
+          #format.html { redirect_to controller: :accounts, action: :show, id: @user.id }
+          #format.js
+        #end
       end
 
   def destroy
