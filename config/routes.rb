a5308dd8907448f324e405df00808284bab3d028
yuta higa <yh19950420@gmail.com>
2022-03-24 22:23:26 +0900

    deviseのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    #post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     devise_for :users, controllers: { registrations: 'users/registrations' }
   end
   root to: 'homes#index'
