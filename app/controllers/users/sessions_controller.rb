e55106bce07b0fb9b02e78968372473c74d91ba3
yuta higa <yh19950420@gmail.com>
2022-03-24 22:56:22 +0900

    ゲストログインの設定を変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,11 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
-    
     user = User.find(1)  #ゲストユーザー（id:28）にログインさせている
-    user.update(email: 'guest@example.com', name: 'ゲストユーザー') do |user|
-    user.password = SecureRandom.urlsafe_base64
-  end
-    sign_in user  #ユーザーにログインさせる
+    user.update(email: 'guest@example.com', name: 'ゲストユーザー') do |u|
+      u.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user #ユーザーにログインさせる
     #redirect_to user_path(id: current_user.id)
     redirect_to root_path
   end
