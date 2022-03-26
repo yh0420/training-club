16fead0d0012ae22bc9c55b7282a3d4b895aff14
yuta higa <yh19950420@gmail.com>
2022-03-27 08:46:26 +0900

    ログ確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
+    p "カレントユーザー"
     @current_user = User.find_by(id: session[:user_id])
   end
 
