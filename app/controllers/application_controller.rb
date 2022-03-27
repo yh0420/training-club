477064b17b0ecf4bc693fe9dee9015d203c1d449
yuta higa <yh19950420@gmail.com>
2022-03-27 10:27:56 +0900

    applicationcontrollerを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_user
-  
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
 
   def set_current_user
     p "current_user"
