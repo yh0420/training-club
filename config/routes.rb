fad64b32d142103344667f0011b1a3b7078e26af
yuta higa <yh19950420@gmail.com>
2022-03-27 10:23:40 +0900

    ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     devise_for :users, controllers: { registrations: 'users/registrations' }
